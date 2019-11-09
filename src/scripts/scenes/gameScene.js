5d7fde46f605d8ac31484360765580ae5eaa371a
Tym17 <Tym17@users.noreply.github.com>
2019-11-09 15:44:21 +0100

    added grid

--- src/scripts/scenes/gameScene.js
+++ src/scripts/scenes/gameScene.js
@@ -1,12 +1,34 @@
 
+const CELL_SIZE = 96;
+const ITEMGRID_WIDTH = 1248;
+const ITEMGRID_HEIGTH = 768;
+
 export default class GameScene extends Phaser.Scene {
+    debugGrid
+    mainGrid
 
     constructor() {
         super({ key: 'GameScene' });
     }
 
+    preload() {
+        this.load.spritesheet('furnitures', 'assets/sprites/skorjund.png', { frameWidth: 96, frameHeight: 96 });
+
+    }
+
     create() {
         console.log('Gamescene started');
+        this.debugGrid = this.add.grid(this.cameras.main.width * 0.4,
+                                    this.cameras.main.height * 0.4, 
+                                    ITEMGRID_WIDTH, ITEMGRID_HEIGTH,
+                                    CELL_SIZE, CELL_SIZE, 
+                                0xc9c9c9, 1, 0x0000FF);
+
+
+
+    
+        
+        
     }
 
     update() {
